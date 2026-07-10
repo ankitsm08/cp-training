25a8322b1bf0ac71b631348f1ba5a2075386d6df
Ankit Sharma <ankitsm08@gmail.com>
2026-07-11 03:30:42 +0530

    init: add clang settings and base cpp template

---
+++ templates/base.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+void solve() {
+  // Solve
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
