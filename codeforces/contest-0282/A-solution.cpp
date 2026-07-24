9d97e3ff127b1804b946ca384a2fdf384ac06ac0
Ankit Sharma <ankitsm08@gmail.com>
2026-07-24 22:52:52 +0530

    solve: 0282A (800)

---
+++ codeforces/contest-0282/A-solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t = 1;
+  cin >> t;
+
+  int x = 0;
+  while (t--) {
+    char c1, c2, c3;
+    cin >> c1 >> c2 >> c3;
+
+    x += '+' - c2 + 1;
+  }
+
+  cout << x << '\n';
+
+  return 0;
+}
